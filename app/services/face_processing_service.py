b273e0bb66b620956ccd35bd46b359cd3e49d3b6
nikola.r <nikolariorovic@hotmail.com>
2025-04-10 14:15:19 +0200

    Enhance logging configuration in FaceProcessingService and RecognitionService to include timestamp and log level for improved clarity and debugging.

--- app/services/face_processing_service.py
+++ app/services/face_processing_service.py
@@ -8,6 +8,7 @@ from PIL import Image as PILImage
 from deepface import DeepFace
 import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 class FaceProcessingService:
--- app/services/recognition_service.py
+++ app/services/recognition_service.py
@@ -6,6 +6,7 @@ from deepface import DeepFace
 from PIL import Image
 import numpy as np
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 class RecognitionService:
