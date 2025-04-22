8b3487833169dd6d5f6ca09ed0279135d11f349e
nikola.r <nikolariorovic@hotmail.com>
2025-04-22 15:42:59 +0200

    Enhance WasabiService with improved logging and method refactoring
    - Added logging capabilities to track uploads in the upload_to_s3 method, enhancing traceability.
    - Refactored upload_to_s3 method to use the static get_s3_client method for better clarity and consistency.

--- app/services/wasabi_service.py
+++ app/services/wasabi_service.py
@@ -2,10 +2,15 @@ from dotenv import load_dotenv
 import os
 import json
 import boto3
+import logging
+from botocore.exceptions import NoCredentialsError
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+
 class WasabiService:
+    @staticmethod
     def get_s3_client():
         return boto3.client(
             's3',
@@ -15,9 +20,9 @@ class WasabiService:
             region_name=os.getenv("S3_DEFAULT_REGION")
         )
         
-    def upload_to_s3(local_path: str, s3_bucket: str, s3_key: str):
+    def upload_to_s3(self, local_path, s3_bucket, s3_key):
         logger.info(f"Uploading {local_path} to S3 bucket: {s3_bucket}, key: {s3_key}")
-        s3 = get_s3_client()
+        s3 = WasabiService.get_s3_client()
         try:
             s3.upload_file(local_path, s3_bucket, s3_key)
             logger.info(f"Successfully uploaded {local_path} to S3 as {s3_key}")
