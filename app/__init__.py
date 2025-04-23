cd453cc55c92be1fc8325c71e2c6a49b7ebc72de
nikola.r <nikolariorovic@hotmail.com>
2025-04-23 14:32:58 +0200

    Add configuration for maximum request size in app initialization
    - Increased the maximum request size to 30MB in the Flask app configuration to accommodate larger file uploads.
    - This change ensures that the application can handle larger image transfers without encountering size-related errors.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ from app.routes.admin_routes import admin_routes
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
+    # Povećanje maksimalne veličine zahteva na 30MB
+    app.config['MAX_CONTENT_LENGTH'] = 30 * 1024 * 1024  # 30MB
+    
     # Registrujemo rute
     app.register_blueprint(image_routes)
     app.register_blueprint(admin_routes, url_prefix='/admin')
